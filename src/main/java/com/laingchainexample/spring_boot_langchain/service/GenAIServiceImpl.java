4a75677fb75d41005bb63fc742634394e5633c7e
Yash Kambaria <yashkambaria245@gmail.com>
2025-10-19 13:38:51 +0530

    Sytem message + User Message

--- src/main/java/com/laingchainexample/spring_boot_langchain/service/GenAIServiceImpl.java
+++ src/main/java/com/laingchainexample/spring_boot_langchain/service/GenAIServiceImpl.java
@@ -1,10 +1,16 @@
 package com.laingchainexample.spring_boot_langchain.service;
 
 import com.laingchainexample.spring_boot_langchain.dto.ChatRequest;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -20,11 +26,14 @@ public class GenAIServiceImpl implements GenAIService{
 	
 	@Override
 	public String getResponse(ChatRequest request) {
+		List<ChatMessage> messages=new ArrayList<>();
+		messages.add(SystemMessage.systemMessage("Respond in Hindi"));
+		messages.add(UserMessage.userMessage(request.question()));
 		var model = OpenAiChatModel.builder()
 				.apiKey(apiKey)
 				.baseUrl(baseUrl)
 				.modelName(modelName)
 				.build();
-		return model.chat(request.question());
+		return model.chat(messages).aiMessage().text();
 	}
 }
